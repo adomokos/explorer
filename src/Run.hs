9310ee8e0d569f1ca710d78a1493e05d10408643
Attila Domokos <adomokos@gmail.com>
2019-06-25 23:18:53 -0500

    Fetch public SSH key for a user

--- src/Run.hs
+++ src/Run.hs
@@ -5,15 +5,31 @@ module Run
   )
 where
 
+import Data.List (intercalate)
+import qualified Data.Vector as DV
 import qualified GitHub as GH
+import qualified GitHub.Endpoints.Users.PublicSSHKeys as PK
 import Import
 
 run :: RIO App ()
 run = do
   logInfo "We're inside the application!"
+  fetchUser "adomokos"
+  fetchPublicSSHKey "adomokos"
 
-  eUser <- liftIO $ GH.executeRequest' $ GH.userInfoForR "adomokos"
+fetchPublicSSHKey :: GH.Name GH.Owner -> RIO App ()
+fetchPublicSSHKey username = do
+  ePublicSSHKeys <- liftIO $ PK.publicSSHKeysFor' username
+  case ePublicSSHKeys of
+    (Left  err          ) -> logInfo $ "Error: " <> displayShow err
+    (Right publicSSHKeys) -> logInfo . displayShow $ intercalate "\n" $ map
+      show
+      (DV.toList publicSSHKeys)
+
+fetchUser :: GH.Name GH.User -> RIO App ()
+fetchUser username = do
+  eUser <- liftIO . GH.executeRequest' $ GH.userInfoForR username
 
   case eUser of
-    Left  err  -> logInfo $ "error occurred: " <> displayShow err
+    Left  err  -> logInfo $ "Error: " <> displayShow err
     Right user -> logInfo . displayShow $ user
