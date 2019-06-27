1e3a8797d714f8acafa14e6f9aa79e7a4bcb4b83
Attila Domokos <adomokos@gmail.com>
2019-06-27 17:59:32 -0500

    Run fetch actions async

--- src/Run.hs
+++ src/Run.hs
@@ -1,26 +1,49 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Run
-  ( run
-  )
-where
+  -- ( run
+  -- )
+  where
 
 import qualified Data.Vector as DV
 import qualified GitHub as GH
+import qualified GitHub.Endpoints.Repos as GH
 import qualified GitHub.Endpoints.Users.PublicSSHKeys as PK
 import Import
 
 run :: RIO App ()
 run = do
-  logInfo "We're inside the application!"
-  logInfo =<< showEither <$> fetchUser "adomokos"
-  logInfo =<< showEither <$> fetchPublicSSHKey "adomokos"
+  (user, sshKeys, repo) <- runAsync (fetchUser "adomokos")
+                                    (fetchPublicSSHKeys "adomokos")
+                                    (fetchRepo "adomokos" "light-service")
 
-fetchPublicSSHKey
+  logInfo $ showEither user
+  logInfo $ showEither sshKeys
+  logInfo $ showEither repo
+
+runAsync :: MonadUnliftIO m => m a -> m b -> m c -> m (a, b, c)
+runAsync action1 action2 action3 = do
+  a1 <- async action1
+  a2 <- async action2
+  a3 <- async action3
+  user <- wait a1
+  sshKeys <- wait a2
+  repo <- wait a3
+
+  pure (user, sshKeys, repo)
+
+fetchPublicSSHKeys
   :: (MonadIO m)
   => GH.Name GH.Owner
   -> m (Either GH.Error (DV.Vector GH.PublicSSHKeyBasic))
-fetchPublicSSHKey = liftIO . PK.publicSSHKeysFor'
+fetchPublicSSHKeys = liftIO . PK.publicSSHKeysFor'
+
+fetchRepo
+  :: (MonadIO m)
+  => GH.Name GH.Owner
+  -> GH.Name GH.Repo
+  -> m (Either GH.Error GH.Repo)
+fetchRepo username repo = liftIO $ GH.repository username repo
 
 fetchUser :: (MonadIO m) => GH.Name GH.User -> m (Either GH.Error GH.User)
 fetchUser = liftIO . GH.executeRequest' . GH.userInfoForR
