915893fee3e3a36d35eccb496c6692c38b54c007
Attila Domokos <adomokos@gmail.com>
2019-06-27 18:52:44 -0500

    Fetch repos

--- src/Run.hs
+++ src/Run.hs
@@ -13,22 +13,28 @@ import Import
 
 run :: RIO App ()
 run = do
-  (user, sshKeys, repo) <- runAsync (fetchUser "adomokos")
-                                    (fetchPublicSSHKeys "adomokos")
-                                    (fetchRepo "adomokos" "light-service")
+  ( user,
+    sshKeys,
+    repo) <- runAsync (fetchUser "adomokos")
+                      (fetchPublicSSHKeys "adomokos")
+                      (fetchRepo "adomokos" "light-service")
+                      (fetchRepos "adomokos")
 
   logInfo $ showEither user
   logInfo $ showEither sshKeys
   logInfo $ showEither repo
+  -- logInfo $ showEither repos
 
-runAsync :: MonadUnliftIO m => m a -> m b -> m c -> m (a, b, c)
-runAsync action1 action2 action3 = do
+runAsync :: MonadUnliftIO m => m a -> m b -> m c -> m d -> m (a, b, c)
+runAsync action1 action2 action3 action4 = do
   a1 <- async action1
   a2 <- async action2
   a3 <- async action3
+  a4 <- async action4
   user <- wait a1
   sshKeys <- wait a2
   repo <- wait a3
+  _repos <- wait a4
 
   pure (user, sshKeys, repo)
 
@@ -47,6 +53,10 @@ fetchRepo username repo = liftIO $ GH.repository username repo
 
 fetchUser :: (MonadIO m) => GH.Name GH.User -> m (Either GH.Error GH.User)
 fetchUser = liftIO . GH.executeRequest' . GH.userInfoForR
+
+fetchRepos :: (MonadIO m) => GH.Name GH.Owner -> m (Either GH.Error (Vector GH.Repo))
+fetchRepos username =
+  liftIO $ GH.userRepos username GH.RepoPublicityOwner
 
 showEither :: (Show a, Show b) => Either a b -> Utf8Builder
 showEither eValue = case eValue of
