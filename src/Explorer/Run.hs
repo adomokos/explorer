7f737d6cedefa3d2e17083d9a0eb46f0877c2af0
Attila Domokos <adomokos@gmail.com>
2020-02-19 20:31:59 -0600

    Add a helper to run DB actions

--- src/Explorer/Run.hs
+++ src/Explorer/Run.hs
@@ -6,6 +6,8 @@ import qualified Database.Persist.Sqlite as DP
 import qualified Explorer.DB as DB
 import qualified Explorer.GitHubService as GHS
 import Explorer.Util (showAndReturnEither)
+import LoadEnv (loadEnv)
+import RIO.Process (mkDefaultProcessContext)
 
 run :: RIO App ()
 run =
@@ -23,3 +25,24 @@ run =
   insertGHMetric =
       either (const $ logInfo "No GitHubMetrics was found")
              (DB.runDB . DP.insert_)
+
+-- | Runs a RIO App
+-- For example runRIOApp $ runDB countPeople :: IO Int
+runRIOApp :: RIO App a -> IO a
+runRIOApp f = do
+  app <- getApp
+  runRIO app f
+
+getApp :: IO App
+getApp = do
+  dbPool <- DB.createDbPool
+  pc <- mkDefaultProcessContext
+
+  loadEnv
+
+  pure App
+    { appLogFunc = mempty
+    , appProcessContext = pc
+    , appOptions = Options { optionsVerbose = False }
+    , appConnPool = dbPool
+    }
